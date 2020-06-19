72023c709d5a34773b63d63e8358d277d6d5fa15
shandysetiawan <shandysetiawanliu@gmail.com>
2020-06-19 23:43:01 +0700

    final commit

--- client/dist/main.1e43358e.js
+++ client/dist/main.1e43358e.js
@@ -13192,7 +13192,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63551" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63745" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
